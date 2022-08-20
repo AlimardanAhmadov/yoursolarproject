f5c81ba23397b51adfc12233b9e5de2cd07c8dc8
Alimardan <alimardan_akhmedov@hotmail.com>
2022-08-20 13:34:16 +0400

    geoip

--- order/models.py
+++ order/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from main.models import TimeStampedModel
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from cart.models import CartItem
+
+User = get_user_model()
+
+ORDER_CHOICES = (("pending", "pending"), ("completed", "completed"))
+
+class Order(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=250, blank=True, null=True)
+    status = models.CharField(max_length=10, choices=ORDER_CHOICES)
+    is_paid = models.BooleanField(default=False)
+
+
+class OrderItem(TimeStampedModel):
+    order = models.ForeignKey(
+        Order, related_name="order_items", on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        CartItem, related_name="product_order", on_delete=models.CASCADE
+    )
+    quantity = models.IntegerField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
