4e05457c2541176fd78a7a2abeddd38f431b74b6
Alimardan <alimardan_akhmedov@hotmail.com>
2022-09-29 10:07:42 +0400

    updated logout view

--- user/views.py
+++ user/views.py
@@ -422,16 +422,6 @@ class LogoutView(ListCreateAPIView):
         except Exception:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    """def post(self, request):
-        try:
-            refresh_token = request.data["refresh_token"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-
-            return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)"""
-
 
 class VerifyEmailView(ListCreateAPIView, VerifyEmailView):
     permission_classes = (permissions.AllowAny,)
