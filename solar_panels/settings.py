9edf461eb579ed37d9c3d9a3512295851feb1da1
Alimardan <alimardan_akhmedov@hotmail.com>
2022-10-01 21:13:00 +0400

    allowed_hosts

--- solar_panels/settings.py
+++ solar_panels/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://sopanel.herokuapp.com/',]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
