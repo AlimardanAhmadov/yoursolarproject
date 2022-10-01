a4c0a7960280ea50502ccd5a08597d48448768fd
Alimardan <alimardan_akhmedov@hotmail.com>
2022-10-01 21:19:13 +0400

    asdds

--- solar_panels/settings.py
+++ solar_panels/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'https://sopanel.herokuapp.com/',]
+ALLOWED_HOSTS = ['https://sopanel.herokuapp.com/', 'localhost:8000']
 
 # Application definition
 
@@ -125,6 +125,38 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'solar_panels.wsgi.application'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
