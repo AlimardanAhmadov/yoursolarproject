98ba9eb7dbb2e99e772b5c22be7a4a9c2e944979
Alimardan <alimardan_akhmedov@hotmail.com>
2022-10-02 17:44:48 +0400

    debug=False

--- solar_panels/settings.py
+++ solar_panels/settings.py
@@ -410,7 +410,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
