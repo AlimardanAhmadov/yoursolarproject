02ec1ff529d2e318970f0cca444ecfca2aa05889
Alimardan <alimardan_akhmedov@hotmail.com>
2022-12-12 14:41:59 +0400

    aasd

--- solar_panels/urls.py
+++ solar_panels/urls.py
@@ -24,7 +24,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('ixJl&!77Nm8#H@ZRXb#J6dL#/', admin.site.urls),
+    path('ixJl77Nm8HZRXbJ6dL/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
