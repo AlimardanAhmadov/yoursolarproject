0beaf63da0afcca66ad33688dbe411f86dc93c5f
Alimardan <alimardan_akhmedov@hotmail.com>
2022-12-12 23:08:26 +0400

    ads

--- product/models.py
+++ product/models.py
@@ -29,7 +29,7 @@ AV_CHOICES = (
 CATEGORY_CHOICES = (
     ('Inverter', 'Inverter'),
     ('Panel', 'Panel'),
-    ('Rails', 'Rails & Clips'),
+    ('Rails & Clips', 'Rails & Clips'),
     ('Fitting', 'Fitting'),
     ('Battery', 'Battery'),
     ('Cables', 'Cables'),
