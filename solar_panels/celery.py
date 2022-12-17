4c30316bd41dd8204b662d88c54f35417573ad73
Alimardan <alimardan_akhmedov@hotmail.com>
2022-12-18 01:21:07 +0400

    asd

--- solar_panels/celery.py
+++ solar_panels/celery.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
 import os
+import ssl
 from celery import Celery
 from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "solar_panels.settings")
 
-app = Celery("solar_panels", broker=settings.BROKER_URL, backend=settings.BACKEND_URL)
+app = Celery("solar_panels", broker=settings.BROKER_URL, backend=settings.BACKEND_URL, broker_use_ssl = { 'ssl_cert_reqs': ssl.CERT_NONE }, redis_backend_use_ssl = { 'ssl_cert_reqs': ssl.CERT_NONE })
 #app = Celery('solar_panels', broker=os.environ['REDIS_URL'])
 
 
